98d112d9b1f44f95bb85564b49ed96aae71b8d45
Gibbon <44937323+atsb@users.noreply.github.com>
2022-03-01 12:46:45 +0100

    part 1 src upload

---
+++ src/config.h
@@ -0,0 +1,30 @@
+/* config.h for MSVC builds.  This is just the config.h created by
+ * configure. */
+
+/* config.h for MSVC builds.  This is just the config.h created by
+ * configure. */
+
+/* Name of package */
+#define PACKAGE "brown-bag-doom"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Brown Bag Doom"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Brown Bag Doom 1.7.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "brown-bag-doom"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.7.0"
+
+/* Change this when you create your awesome forked version */
+#define PROGRAM_PREFIX "brown-bag-"
+
+/* Version number of package */
+#define VERSION "1.7.0"
+
